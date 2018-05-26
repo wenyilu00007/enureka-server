58c932bf898882cbaf364f896cf6c71b09f14c22
wenyilu00007 <312873957@qq.com>
2018-05-26 16:51:16 +0800

    服务中心

---
+++ src/main/java/com/example/enurekaserver/EnurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.enurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnurekaServerApplication.class, args);
+	}
+}
